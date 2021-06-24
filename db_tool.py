3c4c9a21b219a26fdae507da04a1c22ef02533bd
Miikka Lehikoinen <mlehikoi@yahoo.com>
2021-06-24 04:28:03 +0000

    DB benchmarking

--- db_tool.py
+++ db_tool.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 import db.models
 from flask import Flask
+from db.util import get_readings
+import timeit
+
+
+def read_all():
+    r = get_readings(limit=100000)
+    # print('Database initialized', len(r))
 
 
 def main():
     app = Flask(__name__)
     db.models.init_db(app)
-    db.models.db.create_all()
-    print('Database initialized')
+    #db.models.db.create_all()
+    print (timeit.timeit(
+        stmt = read_all,
+        number = 2))
 
 
 if __name__ == "__main__":
